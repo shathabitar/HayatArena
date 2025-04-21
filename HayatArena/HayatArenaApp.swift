6cd66f86bdc891aeb6287a1ad1663ca3564be2d4
shathabitar <shathah28@outlook.com>
2025-04-21 17:47:19 +0300

    Initial Commit

---
+++ HayatArena/HayatArenaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HayatArenaApp.swift
+//  HayatArena
+//
+//  Created by Shatha Bitar on 21/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct HayatArenaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HayatArenaTests/HayatArenaTests.swift
@@ -0,0 +1,16 @@
+//
+//  HayatArenaTests.swift
+//  HayatArenaTests
+//
+//  Created by Shatha Bitar on 21/04/2025.
+//
+
+import Testing
+
+struct HayatArenaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
